7fbee38346168629765d9a986d01412f3d3ec18d
Makcim <103420334+makcik303123@users.noreply.github.com>
2023-09-06 12:12:21 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const hour = document.getElementById("hour");
 
 // Обратный счетчик
 
-const targetDate = new Date("May 31, 2023 00:00:00");
+const targetDate = new Date("May 31, 2024 00:00:00");
 
 function counter() {
   const todayDate = Date.now();
